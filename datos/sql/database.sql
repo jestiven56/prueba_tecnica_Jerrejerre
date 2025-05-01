4a6b5a1a433947911f31c77615fb8b0da42bba01
jestiven56 <jestiven56@gmail.com>
2025-05-01 08:52:27 -0500

    feat: agregar tabla de revisores y relacionar con tabla usuarios en script database sql

--- datos/sql/database.sql
+++ datos/sql/database.sql
@@ -12,3 +12,22 @@ CREATE TABLE IF NOT EXISTS usuarios (
     fecha_carga TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Crear tabla de revisores
+CREATE TABLE IF NOT EXISTS revisores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(45),
+    apellido VARCHAR(45)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor1');
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor2');
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor3');
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor4');
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor5');
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor6');
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor7');
+INSERT INTO `revisores` (`nombre`, `apellido`) VALUES ('Nombre', 'Revisor8');
+
+ALTER TABLE usuarios ADD COLUMN revisor_id INT DEFAULT NULL;
+ALTER TABLE usuarios ADD FOREIGN KEY (revisor_id) REFERENCES revisores(id) ON DELETE SET NULL ON UPDATE CASCADE;
